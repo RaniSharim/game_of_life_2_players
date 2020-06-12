e86457c714c2273e61c71c51c9119eecd2078794
rsharim <rani.sharim@gmail.com>
2020-06-12 18:23:01 +0300

    Added single cell coloring

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
+import { Board } from "./board"
+import { Game } from "./game";
 import { UI } from "./ui";
 
-const ui = new UI();
+const board = new Board();
+const game = new Game(board);
+const ui = new UI(game);
+
 ui.create_board();
